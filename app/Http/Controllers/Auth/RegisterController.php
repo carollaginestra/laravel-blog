6ed0cc05b23201c7d93f0a44a6046d050763e831
Carolina <carolinalaginestra@hotmail.com>
2019-01-01 21:28:23 -0200

    fix: Changing redirection route Changing redirection route when user is registered

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/painel/usuarios';
 
     /**
      * Create a new controller instance.
