dc9c227d7e245d43e00057e51d9dc61d8016e61a
Carolina <carolinalaginestra@hotmail.com>
2019-01-01 23:58:33 -0200

    fix: adding responsive img class and changing button name

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -39,50 +39,54 @@
 <section class="content">
     <div class="col-md-8">
         
-        <?php for($i = 1; $i <= 10; $i++){?>
+        @forelse($datas as $key)
         <article class="post">
-            <div class="image-post col-md-4 text-center">
-                <img src="imgs/img1.jpg" alt="Nome Post" class="img-post">
+            <div class="col-md-3 text-center">
+                <div class="image-post">
+                    <img src="{{URL::asset('/assets/uploads/posts/'.$key->image)}}"
+                        alt="{{ $key->title }}" class="img-post">
+                </div>
             </div>
-            <div class="description-post col-md-8">
-                <h2 class="title-post">Título do post pode vir bem aqui...</h2>
+            <div class="description-post col-md-9">
+                <div>
+                    <h2 class="title-post">{{ $key->title }}</h2>
+                </div>
+                
+                <div>
+                    <p class="description-post">
+                        {{ str_limit($key->description, 600) }}
+                    </p>
+                </div>
 
-                <p class="description-post">
-                    Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração...
-                </p>
-
-                <a class="btn-post" href="?pg=post">Ir <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <div>
+                    <a class="btn-post btn" href="post/{{$key->id}}">
+                        Ver mais <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                </div>
             </div>
         </article>
-        <?php }?>
+        @empty
+            <div class="alert alert-danger" role="alert">
+                <strong>Nenhuma postagem existente...</strong>
+            </div>  
+        @endforelse
 
-        <div class="pagination-posts">
-            <nav aria-label="Page navigation">
-              <ul class="pagination">
-                <li>
-                  <a href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li class="active"><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-                <li>
-                  <a href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-        </div>
+        {{-- {{$datas->links()}} --}}
+
+        @if(isset($dataForm))
+            {{$datas->appends(Request::only('pesquisa'))->links()}}
+        @else
+            {{$datas->links()}}
+        @endif
 
     </div><!--POSTS-->
 
     <!--Sidebar-->
-    <div class="col-md-4">
-    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FfaculdadeAlfaUmuarama%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=316115088513380" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>		</section>
+    <div class="col-md-4 col-xs-12">
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FfaculdadeAlfaUmuarama%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=316115088513380"
+            width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    </div>
+
+</section>
 
 @endsection
-       
